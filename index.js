7d9aa2332c803314c4547884e2f9c1750961462a
Salman K <salman.m.khakwani@gmail.com>
2018-03-20 23:28:32 +0500

    added javascript key

--- index.js
+++ index.js
@@ -6,9 +6,29 @@ var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var appId = process.env.APP_ID;
+var masterKey = process.env.MASTER_KEY;
+var serverURL = process.env.SERVER_URL;
+var clientKey = process.env.CLIENT_KEY;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+if (!appId) {
+  console.log('APP_ID not specified, falling back to localhost.');
+}
+
+if (!masterKey) {
+  console.log('MASTER_KEY not specified, falling back to localhost.');
+}
+
+if (!serverURL) {
+  console.log('SERVER_URL not specified, falling back to localhost.');
+}
+
+if (!clientKey) {
+  console.log('CLIENT_KEY not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
@@ -19,7 +39,8 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  javascriptKey: process.env.JAVASCRIPT_KEY || "myJavascriptKey"
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -34,9 +55,23 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
+app.use(function (req, res, next) {
+  res.set({ // since there is no res.header class in Parse, we use the equivalent to set the response headers
+    'Access-Control-Allow-Origin': '*/*',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, X-Parse-Session-Token'
+  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Parse-Session-Token');
+  next();
+});
+
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('Now-App Backend v0.1');
 });
 
 // There will be a test page available on the /test path of your server url
@@ -48,7 +83,7 @@ app.get('/test', function(req, res) {
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('Now-app backend running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
